abceaa21cf5df9f313febb509813d4f3eba3b846
PietroWilhelm <pietrowilhelm@git.com.br>
2025-04-16 11:26:52 -0300

    Foi realizado a interação com o usúario e teste mocado também

--- Projeto07/src/br/com/fiap/bean/Televisor.java
+++ Projeto07/src/br/com/fiap/bean/Televisor.java
@@ -28,28 +28,26 @@ public class Televisor {
 
     public void setCanal(int canal) {
         try {
-            if (canal == 2 || canal == 4 || canal == 5 || canal == 7 || canal ==13){
+            if (canal == 2 || canal == 4 || canal == 5 || canal == 7 || canal == 13){
                 this.canal = canal;
             }else {
-                throw new Exception("Canal fora faixa permitida! (min=2 até max=13 dentro dos canais 4, 5, 7 e 13 qualquer coisa fora disso não é permitido)");
+                throw new Exception("Canal fora faixa permitida! Canais permitidos: 2, 4, 5, 7, 13");
             }
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
-    // métodos das classe (particulares)
+    // métodos das classe (particulares)ww
     public void aumentarVolume(){
         if (volume < 20){
-
+            volume++;
         }
-        volume++;
     }
 
     public void diminuirVolume(){
         if (volume > 0){
-
+            volume--;
         }
-        volume--;
     }
 }
 
--- Projeto07/src/br/com/fiap/main/Main.java
+++ Projeto07/src/br/com/fiap/main/Main.java
@@ -3,24 +3,50 @@ package br.com.fiap.main;
 import br.com.fiap.bean.Radio;
 import br.com.fiap.bean.Televisor;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("*---------TV-----------*");
+        Scanner scan = new Scanner(System.in);
+        Televisor tv = new Televisor();
+        /*
+        tv.setCanal(17);
+        tv.setVolume(19);
+        tv.aumentarVolume();
+        tv.aumentarVolume();*/
 
-        Radio radio = new Radio();
-        radio.setVolume(99);
-        radio.setEstacao(89.1f);
-        radio.aumentarVolume();
-        radio.aumentarVolume();
-        radio.aumentarVolume();
-        radio.setEstacao(92.5f);
-        System.out.printf("Estação: %.1fMHZ \nVolume: %d", radio.getEstacao(), radio.getVolume());
 
-        Televisor televisor = new Televisor();
-        televisor.setCanal(7);
-        televisor.setVolume(19);
-        televisor.diminuirVolume();
-        televisor.diminuirVolume();
-        televisor.setCanal(13);
-        System.out.printf("\nCanal: %d \nVolume: %d", televisor.getCanal(), televisor.getVolume());
+        int escolha;
+        try {
+
+            System.out.println("Digite um Canal: ");
+            tv.setCanal(scan.nextInt());
+            System.out.println("Digite o volume: ");
+            tv.setVolume(scan.nextInt());
+
+            System.out.println("Faça sua escolha (1)\n definir novo canal\n (2) definir novo volume \n(3) aumentar volume \n(4) Diminir volume");
+            escolha = scan.nextInt();
+
+            if (escolha == 1){
+                System.out.println("Digite um novo canal");
+                tv.setCanal(scan.nextInt());
+            }else if (escolha == 2){
+                System.out.println("Digite um novo volume");
+                tv.setVolume(scan.nextInt());
+            }else if (escolha == 3){
+                System.out.println("Aumentar volume");
+                tv.setVolume(scan.nextInt());
+            }
+            else{
+                System.out.println("Diminuir volume");
+                tv.setVolume(scan.nextInt());
+            }
+            System.out.printf("\nCanal: %d \nVolume: %d", tv.getCanal(), tv.getVolume());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
